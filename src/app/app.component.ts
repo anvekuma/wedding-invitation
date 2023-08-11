c810669d7019ce212a8b9849daa346c27a49a59c
anvekuma <anveshkumar.sayana@brambles.com>
2023-08-12 00:43:45 +0530

    Initial Design

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,13 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'wedding-invitation';
+
+  navigateMarriageLocation(){
+    window.open('https://maps.app.goo.gl/QtK5CoSM8rTpPCDT7');
+  }
+  
+  navigateReceptionLocation(){
+    window.open('https://goo.gl/maps/8MbdY3jPAvK5gC698');
+  }
+
 }
